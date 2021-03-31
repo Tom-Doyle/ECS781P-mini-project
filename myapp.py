362cfacda3519b15f0668ce2a0c6b50ad3fbc376
Tom Doyle <81024704+Tom-Doyle@users.noreply.github.com>
2021-03-31 22:33:03 +0100

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -22,3 +22,17 @@ try:
     )
 except:
     x=1
+
+
+@app.route('/')
+def api_information():
+    # This function returns a HTML page with information on all the other APIs
+    return """<html>
+<body>
+<h1>Investment Restful API Service</h1>
+<h3>Designed, developed and implemented by Freddie Savundra, Christa Dobson and Tom Doyle</h3>
+<b>Group: 34</b>
+<br></br>
+<br></br>
+
+# Next step is adding Get, Put, Post, Delete requests
