9700125d758ca1b5b0ba3fbbb4dd9c30439fce07
Tom Doyle <81024704+Tom-Doyle@users.noreply.github.com>
2021-04-03 14:58:38 +0100

    Create sql_code.sql

---
+++ sql_code.sql
@@ -0,0 +1,7 @@
+/* Command to create the SQL table */ 
+CREATE TABLE music_data (
+pk_track_id INT(6),
+track_title VARCHAR(30) NOT NULL,
+track_artist VARCHAR(30) NOT NULL,
+track_album VARCHAR (30) NOT NULL
+)
